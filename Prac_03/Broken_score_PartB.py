2ce77b12b696a1d6269698fda42ea26dddc2d0d5
Arjun <arjun.baiju@my.jcu.edu.au>
2021-03-28 19:53:02 +1000

    Part B - takes in the user's score as a parameter and returns the result.

---
+++ Prac_03/Broken_score_PartB.py
@@ -0,0 +1,19 @@
+"""
+CP1404/CP5632 - Practical
+Broken program to determine score status
+"""
+def main():
+    score = float(input("Enter score: "))
+    print(check_rank(score))
+
+def check_rank(score):
+    if score < 0 or score > 100:
+        return "Invalid score"
+    elif score >= 90:
+        return "Excellent"
+    elif score >= 50:
+        return "Passable"
+    else:
+        return "Bad"
+
+main()
