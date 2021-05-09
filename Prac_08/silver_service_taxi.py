e38234e2f11c750611e870c83c0819247823514d
ArjunBaiju3 <arjun.baiju@my.jcu.edu.au>
2021-05-09 22:11:58 +1000

    Add silver service taxi class

---
+++ Prac_08/silver_service_taxi.py
@@ -0,0 +1,22 @@
+"""
+SilverServiceTaxi class
+"""
+
+from Prac_08.taxi import Taxi
+
+
+class SilverServiceTaxis(Taxi):
+
+    def __init__(self, name, fuel, fanciness):
+        super().__init__(name, fuel)
+        self.fanciness = fanciness
+        self.price_per_km *= fanciness
+
+    flagfall = 4.50
+
+    def calculate_fare(self):
+        return self.flagfall + super().get_fare()
+
+    def __str__(self):
+        return "{} plus flagfall of ${:.2f}"\
+            .format(super().__str__(), self.flagfall)
