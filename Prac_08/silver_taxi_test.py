37b3d3b4f040e1b8a9b83e2488c4c3af159bc35d
ArjunBaiju3 <arjun.baiju@my.jcu.edu.au>
2021-05-09 22:26:30 +1000

    Add silver service taxi testing

---
+++ Prac_08/silver_taxi_test.py
@@ -0,0 +1,15 @@
+"""
+SilverServiceTaxi class test
+"""
+
+from Prac_08.silver_service_taxi import SilverServiceTaxis
+
+
+def main():
+    taxi = SilverServiceTaxis("Hummer", 100, 2)
+    taxi.drive(18)
+
+    print(taxi)
+    print('$', taxi.get_fare())
+
+main()
