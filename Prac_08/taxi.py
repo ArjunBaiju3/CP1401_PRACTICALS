ad39bb079359cb7df02803e19dae414747a811d9
ArjunBaiju3 <arjun.baiju@my.jcu.edu.au>
2021-05-09 23:00:25 +1000

    Update get_fare method to round to 10c.

--- Prac_08/taxi.py
+++ Prac_08/taxi.py
@@ -1,6 +1,6 @@
 """
 CP1404/CP5632 Practical
-Car class
+Taxi class
 """
 from Prac_08.car import Car
 
@@ -22,7 +22,8 @@ class Taxi(Car):
 
     def get_fare(self):
         """Return the price for the taxi trip."""
-        return self.price_per_km * self.current_fare_distance
+        taxi_fare = (self.price_per_km * self.current_fare_distance)
+        return round(taxi_fare, 1)
 
     def start_fare(self):
         """Begin a new fare."""
