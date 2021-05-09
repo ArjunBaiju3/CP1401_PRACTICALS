05adaf3c97712912fa252eceb7afdded7a938818
ArjunBaiju3 <arjun.baiju@my.jcu.edu.au>
2021-05-09 22:26:06 +1000

    Fix error with flagfall not being added onto total fare

--- Prac_08/silver_service_taxi.py
+++ Prac_08/silver_service_taxi.py
@@ -14,9 +14,9 @@ class SilverServiceTaxis(Taxi):
 
     flagfall = 4.50
 
-    def calculate_fare(self):
+    def get_fare(self):
         return self.flagfall + super().get_fare()
 
     def __str__(self):
-        return "{} plus flagfall of ${:.2f}"\
+        return "{} plus flagfall of $ {:.2f}"\
             .format(super().__str__(), self.flagfall)
